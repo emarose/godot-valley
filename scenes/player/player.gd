f0367b16868209316e7914494b6eb926c12ca5ff
Emanuel Amerio <emanuelamerio.rose@gmail.com>
2026-05-01 15:04:05 -0300

    feat: added basic movement state machine setup

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,11 +5,16 @@ var direction: Vector2
 var speed:= 300
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	get_input()
 	velocity = direction * speed
 	move_and_slide()
+	animation()
 	
 func get_input():
 	direction = Input.get_vector("left","right","up","down")
-	print("direction:",direction)
+
+func animation():
+	if direction:
+		var target_vector: Vector2 = Vector2(round(direction.x), round(direction.y))
+		$AnimationTree.set("parameters/MoveStateMachine/idle/blend_position", target_vector)
